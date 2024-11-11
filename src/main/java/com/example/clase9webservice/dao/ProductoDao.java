d5ab05dd4f940f29b4061543fe30ec4c2375daf3
Pedro Bustamante <a20206156@pucp.edu.pe>
2024-11-11 11:59:22 -0500

    Merge cambios en feature/categories

--- src/main/java/com/example/clase9webservice/dao/ProductoDao.java
+++ src/main/java/com/example/clase9webservice/dao/ProductoDao.java
@@ -1,4 +1,4 @@
-package com.example.clase9webservice.repository;
+package com.example.clase9webservice.dao;
 
 import com.example.clase9webservice.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
