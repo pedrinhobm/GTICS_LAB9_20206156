f5ba33323672909adeea25eddfdc22de8e450cc6
Pedro Bustamante <a20206156@pucp.edu.pe>
2024-11-11 12:43:54 -0500

    Update Category.java

--- src/main/java/com/example/clase9webservice/entity/Category.java
+++ src/main/java/com/example/clase9webservice/entity/Category.java
@@ -9,9 +9,40 @@ import lombok.Setter;
 public class Category {
 
     private int idCategory;
-    private String strCategory;
+    private String strCategoryName;
     private String strCategoryThumb;
     private String strCategoryDescription;
 
+    public int getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(int idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    public String getStrCategoryName() {
+        return strCategoryName;
+    }
+
+    public void setStrCategoryName(String strCategoryName) {
+        this.strCategoryName = strCategoryName;
+    }
+
+    public String getStrCategoryThumb() {
+        return strCategoryThumb;
+    }
+
+    public void setStrCategoryThumb(String strCategoryThumb) {
+        this.strCategoryThumb = strCategoryThumb;
+    }
+
+    public String getStrCategoryDescription() {
+        return strCategoryDescription;
+    }
+
+    public void setStrCategoryDescription(String strCategoryDescription) {
+        this.strCategoryDescription = strCategoryDescription;
+    }
 }
 
