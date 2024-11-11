fc69fe203e83694809bef0ec7474b30ce05a012f
Pedro Bustamante <a20206156@pucp.edu.pe>
2024-11-11 12:39:43 -0500

    Update Category.java

--- src/main/java/com/example/clase9webservice/entity/Category.java
+++ src/main/java/com/example/clase9webservice/entity/Category.java
@@ -4,27 +4,14 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "categories")
 @Getter
 @Setter
-@JsonIgnoreProperties("picture")
 public class Category {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "CategoryID", nullable = false)
-    private Integer id;
 
-    @Column(name = "CategoryName", nullable = false, length = 15)
-    private String categoryName;
-
-    @Lob
-    @Column(name = "Description")
-    private String description;
-
-    @Column(name = "Picture")
-    private byte[] picture;
+    private int idCategory;
+    private String strCategory;
+    private String strCategoryThumb;
+    private String strCategoryDescription;
 
 }
+
