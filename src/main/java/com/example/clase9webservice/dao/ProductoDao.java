0c9bbc287dd618b76f4f3225298106ffea60f14e
Pedro Bustamante <a20206156@pucp.edu.pe>
2024-11-11 15:50:59 -0500

    comit
    comit

--- src/main/java/com/example/clase9webservice/dao/ProductoDao.java
+++ src/main/java/com/example/clase9webservice/dao/ProductoDao.java
@@ -1,6 +1,5 @@
 package com.example.clase9webservice.dao;
 
-import com.example.clase9webservice.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
---
+++ src/main/java/com/example/clase9webservice/entity/MealResponse.java
@@ -0,0 +1,14 @@
+package com.example.clase9webservice.entity;
+import java.util.List;
+
+public class MealResponse {
+    private List<Meal> meals;
+
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
+    }
+}
